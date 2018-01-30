942c43afc2131b67fae883ab499f91576d9b6867
Nichanan Kesonpat <n.kesonpat@gmail.com>
2018-01-30 11:10:45 +0000

    Refactored collection and record models

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -20,19 +20,7 @@ class Collection
     def pick_random_record(ids)
         random_pick = ids.sample
         raw_record_info = @wrapper.get_release(random_pick)
-        title = raw_record_info['title']
-        artist = raw_record_info.artists[0].name
-        year = raw_record_info.year
-        genre = raw_record_info['genres'].join(", ")
-        if raw_record_info['styles'].nil?
-            styles = []
-        else 
-            styles = raw_record_info['styles'].join(", ")
-        end
-        description = raw_record_info['profile']
-        cover_image = raw_record_info.images[0]
-        tracklist = raw_record_info.tracklist
-        picked_record = Record.new(title, artist, year, genre, styles, cover_image, description, tracklist)
+        picked_record = Record.new(raw_record_info)
         picked_record
     end
 
--- app/models/record.rb
+++ app/models/record.rb
@@ -2,18 +2,24 @@ class Record
     
     attr_accessor :title, :artist, :year, :genre, :styles, :cover_image, :description, :tracklist
     
-    def initialize(title, artist, year, genre, styles, cover_image="", description="", tracklist=[])
-        @title = title
-        @artist = artist
-        @year = year
-        @genre = genre
-        @styles = styles
-        @cover_image = cover_image
-        @description = description
-        @tracklist = tracklist
+    def initialize(raw_record_info)
+        parse_raw_info(raw_record_info)
     end
     
-    def 
+    def parse_raw_info(raw_record_info)
+        @title = raw_record_info['title']
+        @artist = raw_record_info.artists[0].name
+        @year = raw_record_info.year
+        @genre = raw_record_info['genres'].join(", ")
+        if raw_record_info['styles'].nil?
+            @styles = []
+        else 
+            @styles = raw_record_info['styles'].join(", ")
+        end
+        @description = raw_record_info['profile']
+        @cover_image = raw_record_info.images[0]
+        @tracklist = raw_record_info.tracklist
+    end
     
     def record_info_dict
         dict = { "title" => @title, "artist" => @artist, "year" => @year, "genre" => genre, "styles" => styles, "img" => cover_image, "description" => @description }
