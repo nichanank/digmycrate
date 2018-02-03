6340f1b22fbbec19df34e3f50f83e5c81d0e3f41
Nichanan Kesonpat <n.kesonpat@gmail.com>
2018-02-03 12:32:17 +0000

    added video to album page

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,7 +5,19 @@ class Artist
     def initialize(artist_id)
         @wrapper = Discogs::Wrapper.new("digmycrate")
         raw_artist_info = @wrapper.get_artist(artist_id)
-        @profile = raw_artist_info.profile
+        @profile = clean_artist_description(raw_artist_info.profile)
+    end
+    
+    private
+    
+    #Fixes broken hyperlinks in the artist profile text.
+    def clean_artist_description(description)
+        if description.nil?
+            cleaned_description = ""
+        else
+            cleaned_description = description.gsub(/(\[a\=\])/,'')
+        end
+        cleaned_description
     end
     
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,9 +1,10 @@
 class Record
     
-    attr_accessor :title, :artist, :artist_discogs_id, :year, :genre, :styles, :cover_image, :description, :tracklist
+    attr_accessor :title, :artist, :artist_discogs_id, :year, :genre, :styles, :cover_image, :description, :tracklist, :youtube_id, :video_title
     
     def initialize(raw_record_info)
         parse_raw_info(raw_record_info)
+        pick_random_video_from_tracklist if !@videos.empty?
     end
     
     private
@@ -22,6 +23,32 @@ class Record
         @description = raw_record_info['profile']
         @cover_image = raw_record_info.images[0]
         @tracklist = raw_record_info.tracklist
+        if raw_record_info['videos'].nil?
+            @videos = []
+        else
+            @videos = raw_record_info['videos']
+        end
+        
+    end
+    
+    def pick_random_video_from_tracklist
+        random_video = @videos.sample
+        @youtube_id = get_youtube_id(random_video.uri)
+        @video_title = random_video.description.titleize
+    end
+    
+    private
+    
+    def get_youtube_id(url)
+        id = ''
+        url = url.gsub(/(>|<)/i,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
+        if url[2] != nil
+            id = url[2].split(/[^0-9a-z_\-]/i)
+            id = id[0];
+        else
+            id = url;
+        end
+        id
     end
     
 end
